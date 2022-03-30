1ee0fc1301c2e467ccfda0344efc9d3e35443e4e
ykpbt7ya82 <102647009+ykpbt7ya82@users.noreply.github.com>
2022-03-30 16:07:29 +0800

    Update content.tsx

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -139,43 +139,14 @@ const Content = () => {
                     height="calc(100vh - 250px)"
                 >
                     <Box flexGrow={1} alignSelf="stretch" pt="100px">
-                        <H2 fontSize="16px" mb="15px">关于</H2>
+                        <H2 fontSize="16px" mb="15px">备注</H2>
                         <VStack alignItems="flex-start" fontSize="16px">
-                            <Text>
-                                Castaila 是一个资源导航网站，精选国内外优质网站，
-                                让每个人都能找到自己需要的资源。如果你有比较好的资源，可以通过下方地址提供给我们。
-                            </Text>
-                            <Text>
-                                Castalia 是 Afterwork 中的一个项目，想了解更多其他项目可以点击下方图片。
-                            </Text>
-                            <Link
-                                href="https://afterwork-design.github.io"
-                            >
-                                <Image
-                                    src="./afterwork.png"
-                                    m="15px 0"
-                                />
-                            </Link>
+                       
                             <Text>
                                 <b>我的</b>面板中所有内容都是存储在浏览器本地的，所以更换电脑或者浏览器，并不会同步数据。
                                 你可以点击卡片右上角的复选框将你喜欢的网站添加至<b>我的</b>。
                             </Text>
-                            <Text>
-                                项目地址：
-                                <a className="linkColor" href="https://github.com/afterwork-design/castalia">castalia | github</a>
-                            </Text>
-                            <Text>
-                                资源推荐入口：
-                                <a className="linkColor" href="https://github.com/afterwork-design/castalia/issues?q=label%3A%E8%B5%84%E6%BA%90%E6%8E%A8%E8%8D%90+">github 上提交 issue</a>
-                            </Text>
-                        </VStack>
-                    </Box>
-                    <Text color="#999999" fontSize="16px">
-                        <span>Designed by </span>
-                        <a href="https://tangweijuan.com" target="_blank">Tang Weijuan</a>
-                        <span> & Developed by </span>
-                        <a href="https://pengfeixc.com" target="_blank">Wang Pengfei</a>
-                    </Text>
+     
                 </VStack>
             </VStack>
             <AddResourceDrawer open={addResourceModalOpen} close={() => setAddResourceModalOpen(false)} />
